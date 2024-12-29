49cfddd10ec08a26362e8d78057808883d8d896b
somnathdas76 <57346047+somnathdas76@users.noreply.github.com>
2024-12-29 10:59:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Somnath Das",
+    author_email= "somnath.das76@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
